89394b3b78aaa5b1895b7cc2b124b0a493e29617
Ugo Stephant <ugo@poool.fr>
2020-05-19 22:53:44 +0200

    chore: clean native example

--- examples/default/babel.config.js
+++ examples/default/babel.config.js
@@ -1,11 +1,11 @@
 const path = require('path');
 
 module.exports = {
-  presets: ['module:@haul-bundler/babel-preset-react-native'],
+  presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     ['module-resolver', {
       alias: {
-        '@poool/react-native-access': path.resolve('../../src'),
+        '@poool/react-native-access': path.resolve('../../'),
       },
     }],
   ],
