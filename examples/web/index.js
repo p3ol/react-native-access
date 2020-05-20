0727bfacaba543a4c2f3ca686b5af2c4694cc4f3
Ugo Stephant <ugo@poool.fr>
2020-05-20 08:19:02 +0200

    refactor: rework web example structure

--- examples/web/index.js
+++ examples/web/index.js
@@ -1,14 +1,7 @@
 import React from 'react';
 import { AppRegistry, View, Text } from 'react-native';
 
-import { Paywall } from '@poool/react-native-access';
-
-const App = () => (
-  <View>
-    <Text>Here&apos;s the paywall:</Text>
-    <Paywall />
-  </View>
-);
+import App from '../base/App';
 
 AppRegistry.registerComponent('App', () => App);
 AppRegistry.runApplication('App', { rootTag: document.getElementById('app') });
--- examples/web/webpack.config.js
+++ examples/web/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './index.js',
   resolve: {
     alias: {
       'react-native$': 'react-native-web',
@@ -24,7 +24,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      template: './index.html',
       inject: true,
     }),
   ],
