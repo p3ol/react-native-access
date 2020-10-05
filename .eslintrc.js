d022ac45431d923192d2ccc38d3e509e57740432
Ugo Stephant <ugo@poool.fr>
2020-10-05 17:15:26 +0200

    chore: fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,8 @@
 module.exports = {
   extends: ['@poool/eslint-config-react-native'],
+  rules: {
+    'react/prop-types': [2, { ignore: ['className', 'children'] }],
+  },
   overrides: [{
     files: ['tests/**/*.js'],
     parser: 'babel-eslint',
