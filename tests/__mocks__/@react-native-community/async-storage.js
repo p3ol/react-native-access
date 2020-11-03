4957e800e33bf2a9adfbdd809ba8ba370a948a89
defless <simon.deflesschouwer@mmibordeaux.com>
2020-11-03 15:58:15 +0100

    fix(mocks): fix dysfunctional asyncStorage mock

--- tests/__mocks__/@react-native-community/async-storage.js
+++ tests/__mocks__/@react-native-community/async-storage.js
@@ -1,2 +1,6 @@
-export
-  default from '@react-native-community/async-storage/jest/async-storage-mock';
+export default {
+  cookies: {},
+  getItem: function (name) { return this.cookies[name]; },
+  setItem: function (name, value) { this.cookies[name] = value; },
+  removeItem: function (name) { delete this.cookies[name]; },
+};
