c1761c03c69d271bf704fec0693815eed6f8c983
defless <simon.deflesschouwer@mmibordeaux.com>
2020-11-05 15:00:19 +0100

    test(lib): rework tests for Signture component

--- tests/Signature.test.js
+++ tests/Signature.test.js
@@ -1,34 +1,21 @@
 import React from 'react';
-import { Text, Linking } from 'react-native';
-import { render, waitFor, fireEvent } from '@testing-library/react-native';
-import sinon from 'sinon';
+import { Text } from 'react-native';
+import { render, fireEvent } from '@testing-library/react-native';
 
 import Signature from '../src/components/Signature';
 import PaywallContext from '../src/components/PaywallContext';
 
 describe('<Signature />', () => {
-  beforeEach(() => {
-    jest.restoreAllMocks();
-  });
   it('should render without issues', async () => {
     const onSubscribeClick = jest.fn();
-
-    const { getByTestId } = render(
-      <PaywallContext onSubscribeClick={onSubscribeClick}>
+    const { findByTestId } = render(
+      <PaywallContext events={{ onsubscribeclick: onSubscribeClick }}>
         <Text>Test Text</Text>
         <Signature/>
       </PaywallContext>
     );
-
-    await waitFor(() => getByTestId('signatureButton'));
-
-    const signatureButton = getByTestId('signatureButton');
-    Linking.openUrl = sinon.spy();
+    const signatureButton = await findByTestId('signatureButton');
     fireEvent.press(signatureButton);
-
     expect(onSubscribeClick.mock.calls.length).toBe(1);
-    // expect(Linking.openUrl.called).toBe(true);
-    // Does not detect the restoration of Mocks
-
   });
 });
