9b6023fbc0ae7205f745d562c608c6cf46c6dd7e
Ugo Stephant <ugo@poool.fr>
2025-03-05 10:20:40 +0100

    feat: prepare for ios masterpiece

--- ios/PaywallView.mm
+++ ios/PaywallView.mm
@@ -7,6 +7,8 @@
 
 #import "RCTFabricComponentsPlugins.h"
 
+#import <AccessIOS/AccessIOS-Swift.h>
+
 using namespace facebook::react;
 
 @interface PaywallView () <RCTPaywallViewViewProtocol>
@@ -14,12 +16,21 @@ using namespace facebook::react;
 @end
 
 @implementation PaywallView {
-    UIView * _view;
+  UIView * _view;
+  NSString * appId;
+  NSString * pageType;
+  NSString * displayMode;
+  NSDictionary * config;
+  NSDictionary * styles;
+  NSDictionary * texts;
+  NSDictionary * variables;
+  
+  Access * access;
 }
 
 + (ComponentDescriptorProvider)componentDescriptorProvider
 {
-    return concreteComponentDescriptorProvider<PaywallViewComponentDescriptor>();
+  return concreteComponentDescriptorProvider<PaywallViewComponentDescriptor>();
 }
 
 - (instancetype)initWithFrame:(CGRect)frame
@@ -36,6 +47,108 @@ using namespace facebook::react;
   return self;
 }
 
+- (void)reinit {
+  if (appId == nil || pageType == nil || config == nil || styles == nil ||
+      texts == nil || variables == nil || displayMode == nil) {
+    return;
+  }
+
+  if (access != nil) {
+    [access destroy];
+    access = nil;
+  }
+
+  [Access setDebug:[self.config[@"debug"] boolValue]];
+  self.access = [[Access alloc] initWithKey:self.appId];
+  [self.access config:self.config];
+  [self.access styles:self.styles];
+  [self.access texts:self.texts];
+  [self.access variables:self.variables];
+
+  [self initEvents];
+
+  if ([self.displayMode isEqualToString:@"bottom-sheet"]) {
+    [self.access createPaywallWithPageType:self.pageType percent:0 completion:^(NSDictionary * _Nonnull result) {
+      if (self.onDismissBottomSheet) {
+        self.onDismissBottomSheet(@{});
+      }
+    }];
+  } else {
+    UIView *subView = [self.access createPaywallWithPageType:self.pageType];
+    subView.frame = self.frame;
+
+    if (subView != nil) {
+      [self addSubview:subView];
+    }
+  }
+}
+
+- (void)initEvents {
+}
+
+- (void)setAppId:(NSString *)appId {
+  _appId = appId;
+  [self reinit];
+}
+
+- (NSString *)getAppId {
+  return _appId;
+}
+
+- (void)setPageType:(NSString *)pageType {
+  _pageType = pageType;
+  [self reinit];
+}
+
+- (NSString *)getPageType {
+  return _pageType;
+}
+
+- (void)setDisplayMode:(NSString *)displayMode {
+  _displayMode = displayMode;
+  [self reinit];
+}
+
+- (NSString *)getDisplayMode {
+  return _displayMode;
+}
+
+- (void)setConfig:(NSDictionary *)config {
+  _config = config;
+  [self reinit];
+}
+
+- (NSDictionary *)getConfig {
+  return _config;
+}
+
+- (void)setStyles:(NSDictionary *)styles {
+  _styles = styles;
+  [self reinit];
+}
+
+- (NSDictionary *)getStyles {
+  return _styles;
+}
+
+- (void)setTexts:(NSDictionary *)texts {
+  _texts = texts;
+  [self reinit];
+}
+
+- (NSDictionary *)getTexts {
+  return _texts;
+}
+
+- (void)setVariables:(NSDictionary *)variables {
+  _variables = variables;
+  [self reinit];
+}
+
+- (NSDictionary *)getVariables {
+  return _variables;
+}
+
 - (void)updateProps:(Props::Shared const &)props oldProps:(Props::Shared const &)oldProps
 {
     const auto &oldViewProps = *std::static_pointer_cast<PaywallViewProps const>(_props);
@@ -46,26 +159,40 @@ using namespace facebook::react;
     //     [_view setBackgroundColor:[self hexStringToColor:colorToConvert]];
     // }
 
+    if (oldViewProps.appId != newViewProps.appId) {
+      [self setAppId:newViewProps.appId];
+    }
+
+    if (oldViewProps.pageType != newViewProps.pageType) {
+      [self setPageType:newViewProps.pageType];
+    }
+
+    if (oldViewProps.displayMode != newViewProps.displayMode) {
+      [self setDisplayMode:newViewProps.displayMode];
+    }
+
+    if (oldViewProps.config != newViewProps.config) {
+      [self setConfig:newViewProps.config];
+    }
+
+    if (oldViewProps.styles != newViewProps.styles) {
+      [self setStyles:newViewProps.styles];
+    }
+
+    if (oldViewProps.texts != newViewProps.texts) {
+      [self setTexts:newViewProps.texts];
+    }
+
+    if (oldViewProps.variables != newViewProps.variables) {
+      [self setVariables:newViewProps.variables];
+    }
+
     [super updateProps:props oldProps:oldProps];
 }
 
 Class<RCTComponentViewProtocol> PaywallViewCls(void)
 {
-    return PaywallView.class;
-}
-
-- hexStringToColor:(NSString *)stringToConvert
-{
-    NSString *noHashString = [stringToConvert stringByReplacingOccurrencesOfString:@"#" withString:@""];
-    NSScanner *stringScanner = [NSScanner scannerWithString:noHashString];
-
-    unsigned hex;
-    if (![stringScanner scanHexInt:&hex]) return nil;
-    int r = (hex >> 16) & 0xFF;
-    int g = (hex >> 8) & 0xFF;
-    int b = (hex) & 0xFF;
-
-    return [UIColor colorWithRed:r / 255.0f green:g / 255.0f blue:b / 255.0f alpha:1.0f];
+  return PaywallView.class;
 }
 
 @end
--- ios/PaywallViewManager.mm
+++ ios/PaywallViewManager.mm
@@ -14,6 +14,27 @@ RCT_EXPORT_MODULE(AccessView)
   return [[UIView alloc] init];
 }
 
-RCT_EXPORT_VIEW_PROPERTY(color, NSString)
+RCT_EXPORT_VIEW_PROPERTY(appId, NSString)
+RCT_EXPORT_VIEW_PROPERTY(pageType, NSString)
+RCT_EXPORT_VIEW_PROPERTY(displayMode, NSString)
+RCT_EXPORT_VIEW_PROPERTY(config, NSDictionary)
+RCT_EXPORT_VIEW_PROPERTY(styles, NSDictionary)
+RCT_EXPORT_VIEW_PROPERTY(texts, NSDictionary)
+RCT_EXPORT_VIEW_PROPERTY(variables, NSDictionary)
+RCT_EXPORT_VIEW_PROPERTY(onLock, RCTDirectEventBlock)
+RCT_EXPORT_VIEW_PROPERTY(onReady, RCTDirectEventBlock)
+RCT_EXPORT_VIEW_PROPERTY(onRelease, RCTDirectEventBlock)
+RCT_EXPORT_VIEW_PROPERTY(onPaywallSeen, RCTDirectEventBlock)
+RCT_EXPORT_VIEW_PROPERTY(onRegister, RCTDirectEventBlock)
+RCT_EXPORT_VIEW_PROPERTY(onFormSubmit, RCTDirectEventBlock)
+RCT_EXPORT_VIEW_PROPERTY(onSubscribeClick, RCTDirectEventBlock)
+RCT_EXPORT_VIEW_PROPERTY(onLoginClick, RCTDirectEventBlock)
+RCT_EXPORT_VIEW_PROPERTY(onCustomButtonClick, RCTDirectEventBlock)
+RCT_EXPORT_VIEW_PROPERTY(onDataPolicyClick, RCTDirectEventBlock)
+RCT_EXPORT_VIEW_PROPERTY(onAlternativeClick, RCTDirectEventBlock)
+RCT_EXPORT_VIEW_PROPERTY(onError, RCTDirectEventBlock)
+RCT_EXPORT_VIEW_PROPERTY(onAnswer, RCTDirectEventBlock)
+RCT_EXPORT_VIEW_PROPERTY(onDismissBottomSheet, RCTDirectEventBlock)
+RCT_EXPORT_VIEW_PROPERTY(onResize, RCTDirectEventBlock)
 
 @end
