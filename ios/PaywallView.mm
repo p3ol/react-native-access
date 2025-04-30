d1b9c8106044d49fa2df459a3b82fed57aba945e
Ugo Stephant <ugo@poool.fr>
2025-04-30 18:14:16 +0200

    refactor: clean code

--- ios/PaywallView.mm
+++ ios/PaywallView.mm
@@ -26,10 +26,10 @@ using namespace facebook::react;
     NSDictionary * styles;
     NSDictionary * texts;
     NSDictionary * variables;
-    
+
     NSMutableDictionary *_formSubmitObservers;
     NSMutableDictionary *_registerObservers;
-    
+
     Access * access;
 }
 
@@ -56,7 +56,7 @@ using namespace facebook::react;
 
 - (void)reinit {
     NSLog(@"reinit");
-    
+
     if (appId == nil || config == nil) {
         return;
     }
@@ -67,7 +67,7 @@ using namespace facebook::react;
     }
 
     [Access setDebug: [config[@"debug"] boolValue]];
-    
+
     access = [[Access alloc] initWithKey:appId];
     [access config:config :false];
     [access styles:styles :false];
@@ -75,12 +75,12 @@ using namespace facebook::react;
     [access variables:variables];
 
     [self initEvents];
-    
+
     BOOL isBottomSheet = [displayMode isEqualToString:@"bottom-sheet"];
-        
+
     UIView* target = isBottomSheet ? nil : self;
     void (^ _Nullable didSetSize)(CGSize size);
-    
+
     if (!isBottomSheet) {
         didSetSize = ^(CGSize size) {
             PaywallViewEventEmitter::OnResize event = PaywallViewEventEmitter::OnResize { static_cast<int>(size.width), static_cast<int>(size.height) };
@@ -96,7 +96,7 @@ using namespace facebook::react;
 }
 
 - (void)initEvents {
-    
+
     [access onIdentityAvailableWithOnce:false :^(UserEvent * _Nullable event) {
         PaywallViewEventEmitter::OnIdentityAvailable rnEvent = PaywallViewEventEmitter::OnIdentityAvailable {
             [event.widget UTF8String],
@@ -110,7 +110,7 @@ using namespace facebook::react;
         };
         self.eventEmitter.onIdentityAvailable(rnEvent);
     }];
-    
+
     [access onReadyWithOnce:false :^(WidgetEvent * _Nullable event) {
         PaywallViewEventEmitter::OnReady rnEvent = PaywallViewEventEmitter::OnReady {
             [event.widget UTF8String],
@@ -132,7 +132,7 @@ using namespace facebook::react;
         };
         self.eventEmitter.onPaywallSeen(rnEvent);
     }];
-    
+
     [access onSubscribeTappedWithOnce:false :^(ClickEvent * _Nullable event) {
         PaywallViewEventEmitter::OnSubscribeClick rnEvent = PaywallViewEventEmitter::OnSubscribeClick {
             [event.widget UTF8String],
@@ -142,7 +142,7 @@ using namespace facebook::react;
         };
         self.eventEmitter.onSubscribeClick(rnEvent);
     }];
-    
+
     [access onLoginTappedWithOnce:false :^(ClickEvent * _Nullable event) {
         PaywallViewEventEmitter::OnLoginClick rnEvent = PaywallViewEventEmitter::OnLoginClick {
             [event.widget UTF8String],
@@ -152,7 +152,7 @@ using namespace facebook::react;
         };
         self.eventEmitter.onLoginClick(rnEvent);
     }];
-    
+
     [access onDiscoveryLinkTappedWithOnce:false :^(ClickEvent * _Nullable event) {
         PaywallViewEventEmitter::OnDiscoveryLinkClick rnEvent = PaywallViewEventEmitter::OnDiscoveryLinkClick {
             [event.widget UTF8String],
@@ -162,7 +162,7 @@ using namespace facebook::react;
         };
         self.eventEmitter.onDiscoveryLinkClick(rnEvent);
     }];
-    
+
     [access onDataPolicyTappedWithOnce:false :^(ClickEvent * _Nullable event) {
         PaywallViewEventEmitter::OnDataPolicyClick rnEvent = PaywallViewEventEmitter::OnDataPolicyClick {
             [event.widget UTF8String],
@@ -172,7 +172,7 @@ using namespace facebook::react;
         };
         self.eventEmitter.onDataPolicyClick(rnEvent);
     }];
-    
+
     [access onAlternativeTapEventWithOnce:false :^(AlternativeClickEvent * _Nullable event) {
         PaywallViewEventEmitter::OnAlternativeClick rnEvent = PaywallViewEventEmitter::OnAlternativeClick {
             [event.widget UTF8String],
@@ -202,20 +202,20 @@ using namespace facebook::react;
         };
         self.eventEmitter.onCustomButtonClick(rnEvent);
     }];
-    
+
     [access userDidCloseBottomSheet:^{
         PaywallViewEventEmitter::OnDismissBottomSheet rnEvent = PaywallViewEventEmitter::OnDismissBottomSheet {};
         self.eventEmitter.onDismissBottomSheet(rnEvent);
     }];
-    
+
     [access onFormSubmitWithSubmitter:^(FormEvent * _Nonnull event, void (^ _Nonnull method)(NSArray<InvalidForm *> * _Nonnull)) {
         NSString *notifName = @"formSubmitNotification";
         NSNumber *messageId = @(arc4random());
-        
+
         NSString *fieldsStr = [self arrayToString:event.fields.allKeys];
         NSString *valuesStr = [self arrayToString:event.fields.allValues];
         NSString *validStr = [self arrayToString:event.valid.allValues];
-        
+
         id observer = [NSNotificationCenter.defaultCenter addObserverForName:notifName
                                             object:nil
                                             queue:[NSOperationQueue mainQueue]
@@ -223,23 +223,23 @@ using namespace facebook::react;
             [self onFormSubmitNotification:notification messageId:messageId method: method];
         }];
         [self->_formSubmitObservers setObject:observer forKey:messageId];
-        
+
         PaywallViewEventEmitter::OnFormSubmit rnEvent = PaywallViewEventEmitter::OnFormSubmit {
             [fieldsStr UTF8String],
             [valuesStr UTF8String],
             [validStr UTF8String],
             [messageId doubleValue]
         };
-        
+
         self.eventEmitter.onFormSubmit(rnEvent);
     }];
     [access onRegister:^(RegisterEvent * _Nonnull event, void (^ _Nonnull method)(NSString * _Nullable)) {
         NSString *notifName = @"registerNotification";
         NSNumber *messageId = @(arc4random());
- 
+
         NSString *newsletterId = event.newsletterId ? event.newsletterId : @"";
         NSString *passId = event.passId ? event.passId : @"";
-        
+
         id observer = [NSNotificationCenter.defaultCenter addObserverForName:notifName
                                             object:nil
                                             queue:[NSOperationQueue mainQueue]
@@ -247,7 +247,7 @@ using namespace facebook::react;
             [self onRegisterNotification:notification messageId:messageId method: method];
         }];
         [self->_registerObservers setObject:observer forKey:messageId];
-                
+
         PaywallViewEventEmitter::OnRegister rnEvent = PaywallViewEventEmitter::OnRegister {
             [event.email UTF8String],
             [newsletterId UTF8String],
@@ -263,22 +263,22 @@ using namespace facebook::react;
                          method:(void (^ _Nonnull)(NSArray<InvalidForm *> * _Nonnull))method
 {
     id object = notification.userInfo[@"object"];
- 
+
     NSDictionary *dict = [self extractDictFromNotifObject:object];
-    
+
     if (dict[@"_messageId"] == messageId) {
         NSArray *remoteArray = dict[@"data"];
         NSMutableArray *invalidForms = [[NSMutableArray alloc] init];
-        
+
         for (NSDictionary *form in remoteArray) {
             NSString* key = form[@"fieldKey"];
             NSString* message = form[@"message"];
-            
+
             InvalidForm *invalid = [[InvalidForm alloc] initWithFieldKey:key message: message];
             [invalidForms addObject:invalid];
         }
         method(invalidForms);
-        
+
         [NSNotificationCenter.defaultCenter removeObserver:_formSubmitObservers[messageId]];
         [_formSubmitObservers removeObjectForKey:messageId];
     }
@@ -287,22 +287,22 @@ using namespace facebook::react;
 -(void)onRegisterNotification:(NSNotification*)notification
                     messageId:(NSNumber*)messageId
                        method:(void (^ _Nonnull)(NSString * _Nullable))method
-{    
+{
     id object = notification.userInfo[@"object"];
-    
+
     NSDictionary *dict = [self extractDictFromNotifObject:object];
-    
+
     if (dict[@"_messageId"] == messageId) {
         NSArray *remoteArray = dict[@"data"];
-        
+
         NSString* message;
-        
+
         if (dict.count > 0) {
             message = remoteArray[0][@"message"];
         }
-        
+
         method(message);
-        
+
         [NSNotificationCenter.defaultCenter removeObserver:_registerObservers[messageId]];
         [_registerObservers removeObjectForKey:messageId];
     }
@@ -310,12 +310,12 @@ using namespace facebook::react;
 
 -(NSDictionary*)extractDictFromNotifObject:(id _Nullable)object {
     if (object == nil) { return @{}; }
-    
+
     if ([object isKindOfClass:[NSString class]]) {
         NSError *error;
         NSData *dataUTF8 = [(NSString*)object dataUsingEncoding:NSUTF8StringEncoding];
         NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:dataUTF8 options:0 error:&error];
-        
+
         if (!error) { return dict; }
     }
     return @{};
@@ -325,7 +325,7 @@ using namespace facebook::react;
 {
     const auto &oldViewProps = *std::static_pointer_cast<PaywallViewProps const>(_props);
     const auto &newViewProps = *std::static_pointer_cast<PaywallViewProps const>(props);
-            
+
     if (oldViewProps.appId == newViewProps.appId &&
         oldViewProps.pageType == newViewProps.pageType &&
         oldViewProps.displayMode == newViewProps.displayMode &&
@@ -335,17 +335,17 @@ using namespace facebook::react;
         oldViewProps.variables == newViewProps.variables) {
         return;
     }
-    
+
     appId = [[NSString alloc] initWithUTF8String: newViewProps.appId.c_str()];
-    
+
     pageType = [[NSString alloc] initWithUTF8String: newViewProps.pageType.c_str()];
     displayMode = [[NSString alloc] initWithUTF8String: newViewProps.displayMode.c_str()];
-    
+
     config = [self cppStringToDict:newViewProps.config.c_str()];
     styles = [self cppStringToDict:newViewProps.styles.c_str()];
     texts = [self cppStringToDict:newViewProps.texts.c_str()];
     variables = [self cppStringToDict:newViewProps.variables.c_str()];
-    
+
     [self reinit];
     [super updateProps:props oldProps:oldProps];
 }
@@ -356,7 +356,7 @@ using namespace facebook::react;
     NSData *dataUTF8 = [str dataUsingEncoding:NSUTF8StringEncoding];
     NSError *error;
     NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:dataUTF8 options:0 error:&error];
-    
+
     return dict;
 }
 
@@ -366,7 +366,7 @@ using namespace facebook::react;
                                             options:NSJSONWritingPrettyPrinted
                                             error:&error];
     NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
-    
+
     return jsonString;
 }
 
