7fabca2871097092773ba3c5da231b6b3bb41013
Morgan Berger <morgan.berger71@gmail.com>
2026-01-23 17:03:42 +0100

    feat: adds bigger article text

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -22,6 +22,14 @@ export default function App() {
     }
   }, []);
 
+  const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore \
+  magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis \
+  aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat \
+  cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur \
+  adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation \
+  ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore \
+  eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
   useEffect(() => {
     init();
   }, [init]);
@@ -36,7 +44,7 @@ export default function App() {
           <View collapsable={false} style={styles.wrapper}>
             <Text style={styles.title}>Poool Access Example</Text>
             <Snippet>
-              <Text>Synopsis</Text>
+              <Text>Synopsis : {text}</Text>
             </Snippet>
             <RestrictedContent>
               <Text>Full content</Text>
